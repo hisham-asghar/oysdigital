a63a7ead0e206317fab5a139ea8391436930247b
Hisham Asghar <hisham@octacer.com>
2020-05-04 05:40:38 +0500

    Refactoring

--- Admin/Controllers/HomeController.cs
+++ Admin/Controllers/HomeController.cs
@@ -6,9 +6,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Admin.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Admin.Controllers
 {
+    [Authorize]
     public class HomeController : BaseController
     {
 
